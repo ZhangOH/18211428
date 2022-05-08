fb333c1c5bd11697d3585bb6fd18777ed74b35cf
ZhangOH <101439645+ZhangOH@users.noreply.github.com>
2022-05-08 15:39:48 +0800

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -30,8 +30,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(9000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(9000);                       // wait for a second
 }
